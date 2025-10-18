07e5cda7298dac795b745d8f91f9685309383915
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2025-10-18 13:45:54 -0500

    variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  SUPABASE_URL: 'https://ysawrsxpfqkfnivrdjpp.supabase.co',
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzYXdyc3hwZnFrZm5pdnJkanBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAzMzMxNzQsImV4cCI6MjA3NTkwOTE3NH0.7_v3ma3qRdVUo39FARyA3P50PxDUFlbw5fFibz5c34s',
 };
 
 /*
