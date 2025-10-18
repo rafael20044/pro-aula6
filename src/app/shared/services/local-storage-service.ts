a38a0c02817dfbca4b538d39def4e9e847499573
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2025-10-18 15:27:03 -0500

    auth y local storage service

---
+++ src/app/shared/services/local-storage-service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  constructor() { }
+
+  set(key:string, data:any){
+    const save = JSON.stringify(data);
+    localStorage.setItem(key, save);
+  }
+
+  get<T>(key:string){
+    const data = localStorage.getItem(key);
+    return (data) ? JSON.parse(data) as T : null;
+  }
+}
