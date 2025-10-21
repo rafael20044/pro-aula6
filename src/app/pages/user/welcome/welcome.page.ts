934028ca8cb856d1a3cdfac45301146305daf53a
Maria alejandra perez hernandez <mariagale2468@gmail.com>
2025-10-20 19:16:32 -0500

    page welcome

--- src/app/pages/user/welcome/welcome.page.ts
+++ src/app/pages/user/welcome/welcome.page.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.page.html',
   styleUrls: ['./welcome.page.scss'],
-  standalone: false
+  standalone:false,
 })
-export class WelcomePage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class WelcomePage {
+  rules = [
+    {
+      title: 'Sé divertido',
+      description: 'Disfruta del juego y comparte buenas energías con los demás.',
+      icon: 'assets/icon/divertido.png'
+    },
+    {
+      title: 'Sé curioso',
+      description: 'Haz preguntas interesantes y aprende de otros usuarios.',
+      icon: 'assets/icon/preguntas.png'
+    },
+    {
+      title: 'Sé respetuoso',
+      description: 'Respeta todas las opiniones y evita el lenguaje ofensivo.',
+      icon: 'assets/icon/respect.png'
+    },
+    {
+      title: 'Sé sincero',
+      description: 'Responde con honestidad y autenticidad.',
+      icon: 'assets/icon/sinceridad.png'
+    }
+  ];
 }
