e8770c66ad4d938ffd641d965cc33f65a6000448
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2025-11-27 15:44:43 -0500

    cambio del nombre de la app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'pro-aula',
+  appName: 'Questopia',
   webDir: 'www',
   plugins: {
     LocalNotifications:{
