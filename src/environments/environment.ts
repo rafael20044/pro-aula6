da7c5c0ae880cf79acbbcb65c86d03be8d727c69
SamuelOsp <avilaospina1@gmail.com>
2025-11-27 19:48:36 -0500

    Eliminar environment.ts del repositorio público por seguridad

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  SUPABASE_URL: 'https://ysawrsxpfqkfnivrdjpp.supabase.co',
-  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzYXdyc3hwZnFrZm5pdnJkanBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAzMzMxNzQsImV4cCI6MjA3NTkwOTE3NH0.7_v3ma3qRdVUo39FARyA3P50PxDUFlbw5fFibz5c34s',
-  geminiApiKey: 'AIzaSyCV4Y6OyUqjOW68oMfH4u1mr-0-6atA6lc'
-};
